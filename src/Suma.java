e1e2a0ef5939977a6a6ced6b6df2a688626166d7
Gonzalo <gonzalosantiago930@gmail.com>
2022-03-09 11:34:40 -0600

    primer commit

--- src/Suma.java
+++ src/Suma.java
@@ -1,7 +1,15 @@
-
-
-public class Suma {
- int a=3, b=5;
- 
+import java.util.Scanner;
+public class Suma{
+	public static void main(String[] args) {
+		int  a=4; b=3, resul;
+		
+		resul = a+b;
+		
+		System.out prinln("la suma es :" +resul);
+		
+		
+		
+	}
  
 }
+	    
---
+++ src/SumaNumeros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumaNumeros {
+
+	public static void main(String []args) {
+		int  x,y, resultado;
+		 Scanner teclado = new Scanner (System.in);
+		  System.out.println("ingresa el primer numero: \n");
+		  x= teclado.nextInt();
+				  System.out.println("ingresa el segundo numero: \n");
+		  y= teclado.nextInt();
+		  
+		  resultado = x+y;
+		  System.out.println("\t el resultado de la sumas es: "+ resultado);
+				  
+	}
+		
+	}
+	
+	
+	
